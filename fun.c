6f0fd58e164fcd602d22a06e6a98803d8969faca
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-11-21 07:15:02 +0800

    Create fun.c

---
+++ fun.c
@@ -0,0 +1,45 @@
+void fun1() {
+  int a = 20;
+  if (a>10) {
+    return a;
+  }
+  if (a>20) {
+    return a;
+  }
+  if (a>30) {
+    return a;
+  }
+  if (a>40) {
+    return a;
+  }
+  if (a>50) {
+    return a;
+  }
+  if (a>60) {
+    return a;
+  }
+  if (a>70) {
+    return a;
+  }
+  if (a>80) {
+    return a;
+  }
+  if (a>90) {
+    return a;
+  }
+  if (a>100) {
+    return a;
+  }
+  if (a>110) {
+    return a;
+  }
+  if (a>120) {
+    return a;
+  }
+  if (a>130) {
+    return a;
+  }
+  if (a>140) {
+    return a;
+  }
+}
