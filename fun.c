559ca7a59a19a8b720adc7332f474a38b94366e5
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-11-21 07:16:10 +0800

    Bulk insert

--- fun.c
+++ fun.c
@@ -1,45 +1,45 @@
 void fun1() {
   int a = 20;
   if (a>10) {
-    return a;
+    return a + 30;
   }
   if (a>20) {
-    return a;
+    return a + 30;
   }
   if (a>30) {
-    return a;
+    return a + 30;
   }
   if (a>40) {
-    return a;
+    return a + 30;
   }
   if (a>50) {
-    return a;
+    return a + 30;
   }
   if (a>60) {
-    return a;
+    return a + 30;
   }
   if (a>70) {
-    return a;
+    return a + 30;
   }
   if (a>80) {
-    return a;
+    return a + 30;
   }
   if (a>90) {
-    return a;
+    return a + 30;
   }
   if (a>100) {
-    return a;
+    return a + 30;
   }
   if (a>110) {
-    return a;
+    return a + 30;
   }
   if (a>120) {
-    return a;
+    return a + 30;
   }
   if (a>130) {
-    return a;
+    return a + 30;
   }
   if (a>140) {
-    return a;
+    return a + 30;
   }
 }
